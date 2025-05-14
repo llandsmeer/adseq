0f26eb5d28dea3f1bb306e1aa1c78bcb68a68b1b
Lennart Landsmeer <lennart@landsmeer.email>
2025-05-14 22:57:47 +0200

    bitarray32

--- implementations/__init__.py
+++ implementations/__init__.py
@@ -3,6 +3,7 @@ from .do_nothing import *
 from .ring import *
 from .single_spike import *
 from .single_spike_keep import *
+from .bitarray32 import *
 from .bgpq1 import *
 from .lossy_ring import *
 from .fifo_ring import *
