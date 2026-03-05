4e214665a8b148e359aeb2cd395be0a6c04b27f3
Lennart Landsmeer <lennart@landsmeer.email>
2026-03-05 17:41:53 +0100

    poisson error

--- adseq/benchmarks/profile_lossy_queues.py
+++ adseq/benchmarks/profile_lossy_queues.py
@@ -7,15 +7,14 @@ import matplotlib.pyplot as plt
 
 import sys, os
 
-from .. import implementations
+from adseq import implementations
 
 def mkev(lam: float, Nevents: int, key = jax.random.PRNGKey(0)):
-    Ntimesteps = lam * Nevents
-    event_stream = jnp.zeros((Ntimesteps, ), dtype=bool)
-    ts = jnp.round(jnp.cumulative_sum(
-                        jax.random.poisson(key, lam, (Nevents,))
-                        )).astype(int)
-    return event_stream.at[ts].set(True)
+    T = int(1/lam * Nevents)
+    counts = jax.random.poisson(key, lam=lam, shape=(T,))
+    dropped = ((counts > 1) * counts).sum()
+    jax.debug.print('generator lam={} dropped={} N={}', lam, dropped, Nevents)
+    return (counts > 0).astype(jnp.int32)
 
 def spike_drop(QueueT, lam=10, delay=1, Nevents=100, drop=20, key=jax.random.PRNGKey(0)):
     stream = mkev(lam, Nevents, key=key)
@@ -35,33 +34,59 @@ def spike_drop(QueueT, lam=10, delay=1, Nevents=100, drop=20, key=jax.random.PRN
     return drop / expected.sum()
 
 def make_plot(imp, ax):
-    for lam in 10, 20, 50, 200:
-        dly = jnp.arange(1, 2*lam)
+    #for lam in 0.01, 0.05, 0.1:
+    for lam in [0.1]:
+        dly = jnp.arange(1, int(round(10/lam)))
         o = []
+        key = jax.random.PRNGKey(0)
+        f = jax.jit(lambda delay, key: jax.vmap(lambda delay: spike_drop(imp, lam=lam, delay=delay, Nevents=100, drop=20, key=key))(delay))
         for i in range(3):
-            key=jax.random.PRNGKey(0)
-            f = jax.vmap(lambda delay: spike_drop(imp, lam=lam, delay=delay, Nevents=100, drop=20, key=key))
-            out = f(dly)
+            key, k = jax.random.split(key)
+            out = f(dly, k)
             o.append(out)
         o = jnp.mean(jnp.vstack(o), axis=0)
-        ax.plot(dly/lam, o*100, label=f'$\\lambda$={lam}')
+        ax.plot(dly*lam, o*100, label=f'$\\lambda$={lam}')
     ax.set_title(imp.__name__)
     # ax.legend()
     ax.set_ylabel('Drop rate (%)')
-    ax.set_xlabel('$d/\\lambda$ or $d\\cdot f$ ')
+    ax.set_xlabel('$d \\lambda$')
     ax.set_ylim(0, 100)
 
 
 
 check = [
-    implementations.SingleSpike,
-    implementations.SingleSpikeKeep,
+    # implementations.SingleSpike,
+    # implementations.SingleSpikeKeep,
+    # implementations.FIFORing.sized(2),
+    # implementations.FIFORing.sized(3),
+    # implementations.LossyRing.sized(2),
+    # implementations.LossyRing.sized(3),
+    # implementations.LossyRing.sized(4),
+    # implementations.LossyRing.sized(5),
+    implementations.FIFORing.sized(1),
     implementations.FIFORing.sized(2),
     implementations.FIFORing.sized(3),
-    implementations.LossyRing.sized(2),
-    implementations.LossyRing.sized(3),
-    implementations.LossyRing.sized(4),
-    implementations.LossyRing.sized(5),
+    implementations.FIFORing.sized(4),
+    implementations.FIFORing.sized(5),
+    implementations.FIFORing.sized(6),
+    implementations.FIFORing.sized(7),
+    implementations.FIFORing.sized(8),
+    implementations.FIFORing.sized(9),
+    implementations.FIFORing.sized(10),
+    implementations.FIFORing.sized(11),
+    implementations.FIFORing.sized(12),
+    implementations.FIFORing.sized(13),
+    implementations.FIFORing.sized(14),
+    implementations.FIFORing.sized(15),
+    implementations.FIFORing.sized(16),
+    implementations.FIFORing.sized(17),
+    implementations.FIFORing.sized(18),
+    implementations.FIFORing.sized(19),
+    implementations.FIFORing.sized(20),
+    implementations.FIFORing.sized(21),
+    implementations.FIFORing.sized(22),
+    implementations.FIFORing.sized(23),
+    implementations.FIFORing.sized(24),
 ]
 
 if __name__ == '__main__':
@@ -84,7 +109,7 @@ if __name__ == '__main__':
     plt.rcParams['pdf.fonttype'] = 42
     fig.legend(handles, labels, loc='upper center', ncol=4)
     plt.tight_layout()
-    #plt.savefig(f'./img/lossy_queues.svg')
-    #plt.savefig(f'./img/lossy_queues.png')
+    plt.savefig(f'../../img/lossy_queues_v2.svg')
+    plt.savefig(f'../../img/lossy_queues_v2.png')
     plt.show()
 
