e73c99cf7811b2a217b24542dd2d1e4a4acf060b
Lennart Landsmeer <lennart@landsmeer.email>
2026-06-15 15:14:27 +0200

    benchmark id

---
+++ adseq/benchmarks/__init__.py
@@ -0,0 +1,354 @@
+import os
+import jax.numpy as jnp
+import numpy as np
+import jax
+import socket
+import typing
+
+__all__ = 'get_device_id', 'mkrunner', 'set_backend'
+
+def estimate_backend():
+    b = os.environ.get('BACKEND', '').strip()
+    if b:
+        return b
+    hostname = socket.gethostname()
+    if hostname == 'Groqhost1':
+        return 'groq'
+    else:
+        return 'jax'
+
+BACKEND: typing.Literal['jax', 'onnxrt', 'groq', 'openvino', 'stablehlo'] = estimate_backend()
+
+def set_backend(backend: typing.Literal['jax', 'onnxrt', 'groq', 'openvino', 'stablehlo']):
+    global BACKEND
+    BACKEND = backend
+
+def get_device_id():
+    hostname = socket.gethostname()
+    dev = jax.devices()[0]
+    device = dev.platform
+    hw_version = dev.client.platform_version
+    jax_version = str(jax.__version__)
+    o = dict(hostname=hostname,
+         device=f'{BACKEND}_{device}',
+         hw_version=hw_version,
+         jax_version=jax_version,
+         backend=BACKEND
+         )
+    key =f'{hostname}_{device}_{BACKEND}'
+    print('##########################')
+    print(key)
+    print('##########################')
+    return key, o
+
+def _gather_for_scalar_indexing(args):
+  import tensorflow as tf
+  from jax.experimental.jax2tf import jax2tf
+  indices = tf.expand_dims(args.dnums.start_index_map, 1)
+  op_shape = jax2tf._eval_shape(args.op_shape)
+  slice_sizes_tf = jax2tf._eval_shape(args.slice_sizes)
+  begin = tf.scatter_nd(indices, args.start_indices, [len(op_shape)])
+  end = slice_sizes_tf + begin
+  shrink_mask = sum(2**x for x in args.dnums.collapsed_slice_dims)
+  if slice_sizes_tf == (1,) and len(op_shape) == 1:
+      # XXX HAD TO PUT THIS TO MAKE SHAPES STATIC :(
+      return tf.gather(args.operand, begin)[0]
+  if slice_sizes_tf[-1] == 1 and \
+      slice_sizes_tf[0] == op_shape[0] and \
+      len(slice_sizes_tf) == 2 and \
+      len(op_shape) == 2 and \
+      begin.shape == (2,) and \
+      shrink_mask == 2:
+      # this corresponds to array[0:array.shape[0]-1,idx]
+      return tf.gather(args.operand, begin[1], axis=1)
+      # return  tf.gather(args.operand, begin, axis=1)
+  breakpoint()
+  begin = _clip(op_shape, begin, slice_sizes_tf)
+  res = tf.strided_slice(args.operand, begin, end, shrink_axis_mask=shrink_mask)
+  raise Exception("res has dynamic shape")
+  return res
+
+def _convert_to_tf(f, x, names=None):
+    import tensorflow as tf
+    from jax.experimental import jax2tf
+    jax2tf.impl_no_xla._gather_for_scalar_indexing = _gather_for_scalar_indexing
+    f_tf = jax2tf.convert(f, native_serialization=False, enable_xla=False)
+    if isinstance(x, tuple):
+        sig = [tf.TensorSpec(
+            x.shape if hasattr(x, 'shape') else (),
+            x.dtype if hasattr(x, 'dtype') else np.array(x).dtype,
+            name=f'x{i}' if names is None else names[i]) for i, x in enumerate(x)] # type: ignore
+    else:
+        sig = [tf.TensorSpec(x.shape, x.dtype, name='x')] # type: ignore
+    f_tf = tf.function(f_tf,
+            input_signature=sig,
+            jit_compile=False,
+            autograph=False,
+            reduce_retracing=False,
+
+            )
+    return f_tf, sig
+
+def _convert_to_onnx(f, x, fn='/tmp/runner.onnx', opset=16, names=None):
+    # this doesn't work:
+    # from jax2onnx import to_onnx
+    # return to_onnx(f, x)
+    import tf2onnx
+    f_tf, sig = _convert_to_tf(f, x, names)
+    onnx = tf2onnx.convert.from_function(
+            f_tf,
+            input_signature=sig,
+            output_path=fn,
+            opset=opset)
+    return onnx
+
+def mkrunner_jax(f, x):
+    f = jax.jit(f)
+    f(x).block_until_ready()
+    return lambda: f(x).block_until_ready()
+
+def mkrunner_onnx(f, x):
+    import onnxruntime as rt
+    _convert_to_onnx(f, x)
+    with open('/tmp/runner.onnx', 'rb') as f:
+        model = f.read()
+    sess = rt.InferenceSession(model)
+    arg = dict(x=np.array(x))
+    return lambda: sess.run(None, arg)[0]
+
+def mkrunner_groq(f, x):
+    from groq.runner import tsp
+    _convert_to_onnx(f, x)
+    assert 0 == os.system('groq-compiler -o /tmp/runner /tmp/runner.onnx')
+    assert 0 == os.system('aa-latest --name runner -i /tmp/runner.aa --output-iop /tmp/runner.iop')
+    assert 0 == os.system('iop-utils stats /tmp/runner.iop')
+    program = tsp.create_tsp_runner('/tmp/runner.iop')
+    x_np = np.array(x)
+    k = next(iter(program(x=x_np).keys()))
+    return lambda: program(x=x_np)[k]
+
+def mkrunner_openvino(f, x):
+    import openvino as ov
+    # this doesn't work: convert_model(jax.make_jaxpr(f)(x))
+    shape = ov.Shape(x.shape)
+    f_tf, sig = _convert_to_tf(f, x)
+    del sig
+    model = ov.convert_model(f_tf, shape)
+    model_c = ov.compile_model(model)
+    x_np = np.array(x)
+    return lambda: next(iter(model_c(x_np).values()))
+
+def mkrunner_openvino_loop(f_loop, init, xs, unroll=1000):
+    # [GPU] Unexpected layout of input memory for if:If_466933 node!
+    # Node layout: i32:bfyx:2:nopad
+    # Memory layout: i32:bfyx::nopad
+    import tqdm
+    import openvino as ov
+    import jax.tree
+    # this doesn't work: convert_model(jax.make_jaxpr(f)(x))
+    try:
+        assert isinstance(init, tuple)
+        structure = jax.tree.structure(init)
+        def f_loop_ov(*flat):
+            carry, out = jax.lax.scan(f_loop,
+                    structure.unflatten(flat[:-2]),
+                    (flat[-2], flat[-1]))
+            del out
+            return { str(i): x for i, x in enumerate(carry) }
+        ovsig = tuple(ov.Shape(x.shape if hasattr(x, 'shape') else []) for x in jax.tree.flatten(init)[0]) + \
+                (ov.Shape([unroll]), ov.Shape((unroll,) +  xs[0].shape))
+        sample = tuple(jax.tree.flatten(init)[0]) + (jnp.arange(unroll), xs[0:unroll])
+        sample = tuple(np.array(x) for x in sample)
+        init_np = tuple(np.array(x) for x in jax.tree.flatten(init)[0])
+        f_tf, sig = _convert_to_tf(f_loop_ov, sample)
+        del sig
+        model = ov.convert_model(f_tf, ovsig)
+        model_c = ov.compile_model(model, 'CPU')
+    except Exception as ex:
+        return lambda ex=ex: ex
+    def runner(xs=xs):
+        try:
+            carry = init_np
+            xs = np.array(xs)
+            for i in range(0, len(xs), unroll):
+                x = xs[i:i+unroll]
+                carry = model_c(carry + (np.arange(i, i+unroll), x))
+                carry = tuple(carry[str(i)] for i in range(len(carry)))
+            return carry
+        except Exception as ex:
+            return ex
+    return runner
+
+def build_tag():
+    import inspect
+    # oh well
+    stack = inspect.stack()
+    seen = False
+    target = None
+    for i in range(len(stack)):
+        f = stack[i].function
+        if not seen:
+            if f.startswith('mkrunner'):
+                seen = True
+        else:
+            if not f.startswith('mkrunner'):
+                target = stack[i]
+                break
+    assert target is not None
+    args = [target.function]
+    getargs = inspect.getargvalues(target.frame)
+    for a in getargs.args:
+        q = getargs.locals[a]
+        nm = getattr(q, '__name__', repr(q))
+        nm = nm.replace('[', '_')
+        nm = nm.replace(']', '')
+        args.append(nm)
+    assert getargs.varargs is None
+    assert getargs.keywords is None
+    args = '_'.join(args)
+    return args
+
+def mkrunner_stablehlo(f, x, tag=None):
+    # not an actual runner
+    if tag is None:
+        tag = build_tag()
+    mlir = jax.jit(f).lower(x).as_text()
+    with open(f'hls/{tag}.stablehlo.mlir', 'w') as f:
+        print(mlir, file=f)
+    def runner():
+        raise NotImplementedError('stablehlo')
+
+def mkrunner(f, x, tag=None):
+    match BACKEND:
+        case 'jax':    return mkrunner_jax(f, x)
+        case 'groq':   return mkrunner_groq(f, x)
+        case 'onnxrt': return mkrunner_onnx(f, x)
+        case 'openvino': return mkrunner_openvino(f, x)
+        case 'stablehlo': return mkrunner_stablehlo(f, x)
+    raise Exception('backend not found')
+
+def mkrunner_onnx_loop(f_loop, init, xs, unroll=10):
+    import onnxruntime as rt
+    try:
+        structure = jax.tree.structure(init)
+        def f_loop_unroll(*args):
+            carry_in = structure.unflatten([args[i] for i in range(len(args)-2)])
+            i = args[-2]
+            x = args[-1]
+            carry, out = jax.lax.scan(f_loop,
+                    carry_in, (i, x), unroll=unroll)
+            del out
+            carry = jax.tree.flatten(carry)[0]
+            return { str(i): x for i, x in enumerate(carry) }
+        sample = tuple(jax.tree.flatten(init)[0]) + (jnp.arange(unroll), xs[0:unroll])
+        sample = tuple(np.array(x) for x in sample)
+        init_np = tuple(np.array(x) for x in jax.tree.flatten(init)[0])
+        names = ['C'+str(i) for i in range(len(init_np))] + ['i', 'x']
+        _convert_to_onnx(f_loop_unroll, sample, names=names)
+        with open('/tmp/runner.onnx', 'rb') as f:
+            model = f.read()
+        sess = rt.InferenceSession(model)
+    except Exception as ex:
+        return lambda ex=ex: ex
+    def runner(xs=xs):
+        try:
+            carry = { 'C'+str(i): x for i, x in enumerate(init_np) }
+            xs = np.array(xs)
+            for i in range(0, len(xs), unroll):
+                x = xs[i:i+unroll]
+                i = np.arange(i, i+unroll, dtype='int32')
+                carry['x'] = x
+                carry['i'] = i
+                carry = sess.run(None, carry)
+                carry = { 'C'+str(i): x for i, x in enumerate(carry) }
+            return carry
+        except Exception as ex:
+            return ex
+    return runner
+
+def mkrunner_groq_loop(f_loop, init, xs, unroll=10):
+    assert xs.shape[0] % unroll == 0
+    def recast(x):
+        dt = str(x.dtype)
+        if dt == 'int64':
+            return x.astype('int32')
+        if dt == 'float64':
+            return x.astype('float32')
+        if dt == 'bool':
+            return x.astype('int8')
+        return x
+    from groq.runner import tsp
+    structure = jax.tree.structure(init)
+    def f_loop_unroll(*args):
+        carry_in = structure.unflatten([args[i] for i in range(len(args)-2)])
+        i = args[-2]
+        x = args[-1]
+        if unroll == 1:
+            # o = jax.make_jaxpr(f_loop)(carry_in, (i[0], x[0]))
+            carry, out = f_loop(carry_in, (i[0], x[0]))
+        else:
+            carry, out = jax.lax.scan(f_loop,
+                    carry_in, (i, x), unroll=True)
+        del out
+        carry = jax.tree.flatten(carry)[0]
+        return { str(i): x for i, x in enumerate(carry) }
+    sample = tuple(jax.tree.flatten(init)[0]) + (jnp.arange(unroll, dtype='int32'), xs[0:unroll])
+    sample = tuple(recast(np.array(x)) for x in sample)
+    init_np = tuple(recast(np.array(x)) for x in jax.tree.flatten(init)[0])
+    names = ['C'+str(i) for i in range(len(init_np))] + ['i', 'x']
+    _convert_to_onnx(f_loop_unroll, sample, names=names)
+    assert 0 == os.system('groq-compiler -o /tmp/runner /tmp/runner.onnx')
+    assert 0 == os.system('aa-latest --name runner -i /tmp/runner.aa --output-iop /tmp/runner.iop')
+    assert 0 == os.system('iop-utils stats /tmp/runner.iop')
+    program = tsp.create_tsp_runner('/tmp/runner.iop')
+    init_groq = { 'C'+str(i): recast(x) for i, x in enumerate(init_np) }
+    def runner(xs=xs):
+        try:
+            carry = init_groq
+            xs = np.array(xs)
+            for i in range(0, len(xs), unroll):
+                x = xs[i:i+unroll]
+                i = np.arange(i, i+unroll, dtype='int32')
+                carry['x'] = x
+                carry['i'] = i
+                groq_result = program(**carry)
+                carry = { 'C' + k: v for k, v in groq_result.items() }
+            return carry
+        except Exception as ex:
+            if 'BufferMismatchException' in repr(ex):
+                breakpoint()
+            return ex
+    return runner
+
+def mkrunner_stablehlo_loop(f_loop, init, xs, tag=None):
+    # not an actual runner
+    if tag is None: tag = build_tag()
+    x0 = jnp.array(0, dtype='int32'), xs[0]
+    mlir = jax.jit(f_loop).lower(init, x0).as_text()
+    with open(f'hls/single_iteration_{tag}.stablehlo.mlir', 'w') as f:
+        print(mlir.replace('@main(', '@forward('), file=f)
+    def runner():
+        raise NotImplementedError('stablehlo')
+
+def mkrunner_loop(f_loop, init, xs=None, length=None, **kwargs):
+    assert (xs is None) != (length is None)
+    match BACKEND:
+        case 'openvino': return mkrunner_openvino_loop(f_loop, init, xs)
+        case 'onnxrt': return mkrunner_onnx_loop(f_loop, init, xs)
+        case 'groq': return mkrunner_groq_loop(f_loop, init, xs, unroll=kwargs.get('groq_unroll', None))
+        case 'stablehlo': return mkrunner_stablehlo_loop(f_loop, init, xs, tag=kwargs.get('tag', None))
+    if length is None:
+        f = lambda stream:jax.lax.scan(
+                f=f_loop, # type: ignore
+                init=init,
+                xs=(jnp.arange(len(stream)), stream)
+                )[0][1]
+        return mkrunner(f, xs, tag=kwargs.get('tag', None))
+    else:
+        f = lambda init:jax.lax.scan(
+                f=f_loop, # type: ignore
+                init=init,
+                length=length
+                )[0][1]
+        return mkrunner(f, init, tag=kwargs.get('tag', None))
+
